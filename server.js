bc9ce09a196068fde16d1db0331ba564c4963e06
Gunaseelan R <142430220+Gunaseelan123@users.noreply.github.com>
2026-03-28 21:00:40 +0530

    Set default port to 3000 if not specified

--- server.js
+++ server.js
@@ -66,14 +66,8 @@ if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir, { recursive: true })
 }
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-
-
-
-
-
